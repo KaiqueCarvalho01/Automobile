92f5df4df68ac82311a22a5b06150f288c1c287d
Kaique Carvalho <kaiquecarvalho479jogos@gmail.com>
2025-03-31 10:07:33 -0300

    Enhance form validation; add email and CPF validation logic in js/scripts.js and update alert messages for empty fields

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,16 @@ form.addEventListener("submit", (event) => {
     }
 
     //Verifica se o email está preenchido e se é valido
-    if(emailInput.value === "") {
-        alert("Por favor, preencha o seu email");
+    if(emailInput.value === "" || !validateEmail(emailInput.value)) {
+        alert("Por favor, preencha o seu email corretamente");
+        return;
+    }
+    if(cpfInput.value === "") {
+        alert("Por favor, preencha o seu CPF");
+        return;
+    }
+    if(messageTextarea.value === "") {
+        alert("Por favor, preencha a mensagem");
         return;
     }
 
@@ -26,3 +34,33 @@ form.addEventListener("submit", (event) => {
     //Se todos os campos estiverem corretamente preenchidos, envie o form
     form.submit();
 });
+
+//Função que valida o e-mail
+function validateEmail(email) {
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    );
+    if (emailRegex.test(email)) {
+        return true;
+    }
+    return false;
+}
+
+//Função que valida o CPF
+function validateCPF(cpf) {
+    // Remove caracteres não numéricos
+    cpf = cpf.replace(/[^\d]/g, "");
+
+    // Verifica se o CPF tem 11 dígitos ou é uma sequência repetida (ex: 111.111.111-11)
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+        return false;
+    }
+    
+    const cpfRegex = new RegExp(
+        /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/
+    );
+    if (cpfRegex.test(cpf)) {
+        return true;
+    }
+    return false;
+}
