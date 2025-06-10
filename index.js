edf80ad4106866def648caee5f1c09c2d397f407
Kaique Carvalho <kaiquecarvalho479jogos@gmail.com>
2025-06-10 11:09:46 -0300

    feat(auth): add authentication routes and registration view
    - Implemented routes for user registration, login, and logout in authRoutes.js.
    - Created a registration form view (register.ejs) with fields for name, email, CPF, phone, and password confirmation.
    - Added error handling for registration errors in the view.

--- index.js
+++ index.js
@@ -3,20 +3,56 @@ import db from './config/database.js';
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import session from 'express-session';
+import connectSqlite3 from 'connect-sqlite3';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//Importando rotas
 import propostaRoutes from './routes/propostaRoutes.js';
 import contatoRoutes from './routes/contatoRoutes.js'; // Nova rota de contato
+//Rotas de autenticação
+import authRoutes from './routes/authRoutes.js';
+
+// Configuração do Armazenamento de Sessão com SQLite
+const SQLiteStore = connectSqlite3(session);
+
+// Configuração das Sessões
+app.use(session({
+  store: new SQLiteStore({
+    db: 'db.sqlite',    
+    dir: './config',    
+    table: 'sessions' 
+  }),
+  secret: 'secret-key', // Chave secreta para assinar o cookie da sessão 
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production', // Em produção, usar true com HTTPS
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 // Duração do cookie (ex: 1 dia)
+  }
+}));
+
+// Middleware para expor informações da sessão para todas as views
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = !!req.session.userId; // Verdadeiro se userId existir na sessão
+  res.locals.currentUser = req.session.user || null;  // Informações do usuário logado
+  next();
+});
+
+//rotas de autenticação
+app.use('/auth', authRoutes);
+
 
 
 app.set("view engine", "ejs"); // Configura o EJS como template engine
 app.use(express.static("public")); // Serve arquivos estáticos da pasta public
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 
 // Rotas
@@ -36,7 +72,6 @@ app.get("/estoques", (req, res) => {
 
 // Diz ao app para usar o propostaRoutes para qualquer caminho que comece com /proposta
 app.use('/proposta', propostaRoutes);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
